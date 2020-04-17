92d9c5bf07daed5cc36a0df7058408afb506179d
MichaelRefvem <mkrefvem@gmail.com>
2020-04-16 17:02:46 -0700

    added some for and while loop functions into app.js

--- app.js
+++ app.js
@@ -4,10 +4,8 @@ function getNameAge (){
 var userName = prompt('What is your name?')
 var userAge = prompt('What is your age?')
 
-document.write('<h3>' + userName + '</h3>');
+document.write('<h3>' + 'Hello ' + userName + '</h3>');
 }
-
-'use strict'
 
 function getTimeOfDay (){
 var today = new Date();
@@ -37,6 +35,44 @@ if (r == true) {
 }
 }
 
+function theDogTest (){
+var isTrueDogFan = ''
+
+while ((isTrueDogFan != 'YES') && (isTrueDogFan != 'NO')) {
+  isTrueDogFan = prompt('Are you a true dog fan? (yes or no)').toUpperCase();
+}
+
+if(isTrueDogFan === 'NO')
+  var wannaLearn = prompt('Want to learn about how awesome dogs are?').toUpperCase();
+
+  if (isTrueDogFan === 'YES'){
+    alert('Welcome Dog Fan ' + userName);
+    document.write('<h1>' + userName + 'Welcome to the Best Dog Club!' + '<h1>');
+  }
+
+  else if (wannaLearn === 'no');
+    alert('This site is for Dog Fans only!!!');
+}
+
+
+function numberGuessingGame (){
+  var correctAnswer = 32
+  for(var i = 0; i < 4; i = i + 1){
+    var numberGuess = prompt('Please guess a number between 1 and 50 to find out if you have the potential to be a dog person. You get four tries!')
+    if (numberGuess == correctAnswer) {
+      alert('Correct! You might have a chance!');
+      break;
+    } else {
+      alert('Sorry, please try again!')
+    }
+    if (i == '3') {
+      alert('Sorry! You might actually be more of a cat person.')
+    }
+  }
+}
+
 getNameAge ();
 getTimeOfDay ();
 getDogPreference ();
+theDogTest ();
+numberGuessingGame ();
